cfcf4c17c09c159290e99ac1cca343fbe8324ea0
JuanDLRM <89954046+JuanDLRM@users.noreply.github.com>
2021-10-24 20:32:35 -0500

    Added phoneNumber

--- MySettings-Week 10/ViewController.swift
+++ MySettings-Week 10/ViewController.swift
@@ -13,12 +13,15 @@ class ViewController: UIViewController {
         
         @IBOutlet weak var txtEmail: UITextField!
         
-        @IBOutlet weak var datepicker: UIDatePicker!
+    @IBOutlet weak var phoneNumber: UITextField!
+    
+    @IBOutlet weak var datepicker: UIDatePicker!
         
         @IBAction func btnSave(_ sender: Any) {
             let defaults: UserDefaults = UserDefaults.standard
                 defaults.set(self.txtFullName.text, forKey: "fullname")
                 defaults.set(self.txtEmail.text, forKey: "email")
+                defaults.set(self.phoneNumber.text, forKey: "phoneNumber")
                   let dateFormatter = DateFormatter()
                 dateFormatter.dateFormat = "MMM d, yyyy" //Your New Date format as per requirement change it own
                 let newDate = dateFormatter.string(from:datepicker.date) //pass Date here
@@ -37,6 +40,9 @@ class ViewController: UIViewController {
                   }
                   if defaults.string(forKey: "email") != nil{
                       txtEmail.text = defaults.string(forKey: "email")
+                  }
+                  if defaults.string(forKey: "phoneNumber") != nil{
+                      phoneNumber.text = defaults.string(forKey: "phoneNumber")
                   }
                   if defaults.string(forKey: "bdate") != nil{
                       let bdate  = defaults.string(forKey: "bdate")
